25976002cc4c56af3725aaeab0c7304b007d2a1b
Shifraz Nazar <109841959+ShifrazNazar@users.noreply.github.com>
2024-08-06 12:35:41 +0800

    update profile pop responsive

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -108,7 +108,7 @@ const Profile = () => {
       </main>
 
       {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+        <div className="fixed inset-0 p-10 flex items-center justify-center z-50 bg-black bg-opacity-50">
           <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-md">
             <h2 className="text-2xl font-bold text-primary mb-4">
               Update Username
